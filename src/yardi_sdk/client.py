86e3d52e6057e256d9f4b06d2840d9e838d38240
yhavin <y.havin@gmail.com>
2024-11-21 13:47:17 -0500

    Add basic client framework

---
+++ src/yardi_sdk/client.py
@@ -0,0 +1,30 @@
+import logging
+
+from zeep import Client as ZeepClient
+from zeep.transports import Transport
+from requests import Session
+from requests.auth import HTTPBasicAuth
+
+
+class Client:
+    def __init__(self, wsdl: str, username: str, password: str):
+        session = Session()
+        session.auth = HTTPBasicAuth(username, password)
+        transport = Transport(session=session)
+        self.client = ZeepClient(wsdl=wsdl, transport=transport)
+
+    def call(self, endpoint, log_level=logging.ERROR):
+        logging.getLogger("zeep").setLevel(log_level)
+
+        endpoint_name = endpoint.__class__.__name__
+
+        if not hasattr(self.client.service, endpoint_name):
+            raise ValueError(f"Endpoint '{endpoint_name} not found.")
+        
+        parameters = {key: value for key, value in endpoint.__dict__.items()}
+
+        try:
+            response = getattr(self.client.service, endpoint_name)(**parameters)
+            return response
+        except Exception as e:
+            raise Exception(f"Error calling {endpoint_name}: {e}")
