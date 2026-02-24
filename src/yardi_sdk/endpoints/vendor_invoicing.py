2195849af1c42b475bb3eaa3c5f8224726e2ae20
yhavin <y.havin@gmail.com>
2026-02-24 13:09:50 -0500

    Update vendor invoicing endpoints

--- src/yardi_sdk/endpoints/vendor_invoicing.py
+++ src/yardi_sdk/endpoints/vendor_invoicing.py
@@ -79,6 +79,112 @@ class CancelInvoiceRegisterBatch:
         self.InvoiceNumber = InvoiceNumber
 
 
+class ImportAttachment:
+    """Interface: Vendor Invoicing."""
+    def __init__(
+        self,
+        YardiPropertyId: str,
+        UserName: str = None,
+        Password: str = None,
+        ServerName: str = None,
+        Database: str = None,
+        Platform: str = None,
+        InterfaceEntity: str = None,
+        InterfaceLicense: str = None,
+        InvoiceID: str = None,
+        AttachmentType: str = None,
+        Description: str = None,
+        Attachment: str = None,
+        Extension: str = None
+    ):
+        self.YardiPropertyId = YardiPropertyId
+        self.UserName = UserName or os.getenv("USERNAME")
+        self.Password = Password or os.getenv("PASSWORD")
+        self.ServerName = ServerName or os.getenv("SERVER_NAME")
+        self.Database = Database or os.getenv("DATABASE")
+        self.Platform = Platform or os.getenv("PLATFORM")
+        self.InterfaceEntity = InterfaceEntity or os.getenv("INTERFACE_ENTITY")
+        self.InterfaceLicense = InterfaceLicense or os.getenv("INTERFACE_LICENSE")
+        self.InvoiceID = InvoiceID
+        self.AttachmentType = AttachmentType
+        self.Description = Description
+        self.Attachment = Attachment
+        self.Extension = Extension
+
+
+class GetAttachmentNames:
+    """Interface: Vendor Invoicing."""
+    def __init__(
+        self,
+        YardiPropertyId: str,
+        UserName: str = None,
+        Password: str = None,
+        ServerName: str = None,
+        Database: str = None,
+        Platform: str = None,
+        InterfaceEntity: str = None,
+        InterfaceLicense: str = None,
+        InvoiceID: str = None
+    ):
+        self.YardiPropertyId = YardiPropertyId
+        self.UserName = UserName or os.getenv("USERNAME")
+        self.Password = Password or os.getenv("PASSWORD")
+        self.ServerName = ServerName or os.getenv("SERVER_NAME")
+        self.Database = Database or os.getenv("DATABASE")
+        self.Platform = Platform or os.getenv("PLATFORM")
+        self.InterfaceEntity = InterfaceEntity or os.getenv("INTERFACE_ENTITY")
+        self.InterfaceLicense = InterfaceLicense or os.getenv("INTERFACE_LICENSE")
+        self.InvoiceID = InvoiceID
+
+
+class GetAttachment:
+    """Interface: Vendor Invoicing."""
+    def __init__(
+        self,
+        YardiPropertyId: str,
+        UserName: str = None,
+        Password: str = None,
+        ServerName: str = None,
+        Database: str = None,
+        Platform: str = None,
+        InterfaceEntity: str = None,
+        InterfaceLicense: str = None,
+        InvoiceID: str = None,
+        AttachmentName: str = None
+    ):
+        self.YardiPropertyId = YardiPropertyId
+        self.UserName = UserName or os.getenv("USERNAME")
+        self.Password = Password or os.getenv("PASSWORD")
+        self.ServerName = ServerName or os.getenv("SERVER_NAME")
+        self.Database = Database or os.getenv("DATABASE")
+        self.Platform = Platform or os.getenv("PLATFORM")
+        self.InterfaceEntity = InterfaceEntity or os.getenv("INTERFACE_ENTITY")
+        self.InterfaceLicense = InterfaceLicense or os.getenv("INTERFACE_LICENSE")
+        self.InvoiceID = InvoiceID
+        self.AttachmentName = AttachmentName
+
+
+class GetAttachmentTypes:
+    """Interface: Vendor Invoicing."""
+    def __init__(
+        self,
+        UserName: str = None,
+        Password: str = None,
+        ServerName: str = None,
+        Database: str = None,
+        Platform: str = None,
+        InterfaceEntity: str = None,
+        InterfaceLicense: str = None
+    ):
+        self.UserName = UserName or os.getenv("USERNAME")
+        self.Password = Password or os.getenv("PASSWORD")
+        self.ServerName = ServerName or os.getenv("SERVER_NAME")
+        self.Database = Database or os.getenv("DATABASE")
+        self.Platform = Platform or os.getenv("PLATFORM")
+        self.InterfaceEntity = InterfaceEntity or os.getenv("INTERFACE_ENTITY")
+        self.InterfaceLicense = InterfaceLicense or os.getenv("INTERFACE_LICENSE")
+
+
 class GetPropertyConfigurations:
     """Interface: Vendor Invoicing."""
     def __init__(
